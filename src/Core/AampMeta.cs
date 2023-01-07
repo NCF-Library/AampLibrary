0b3062521a2c77f3d849f9cae09d157a440ddad1
ArchLeaders <archparliament@gmail.com>
2023-01-07 13:53:06 -0800

    WIP AampMeta (writer meta data)

---
+++ src/Core/AampMeta.cs
@@ -0,0 +1,24 @@
+﻿namespace AampLibrary.Core
+{
+    public class AampMeta
+    {
+        private readonly int _ioOffset;
+        public int IoListsOffset => _ioOffset;
+        public int IoObjsOffset => _ioOffset + ListWriters.Count * 12;
+        public int IoParamsOffset => IoObjsOffset + ObjWriters.Count * 8;
+
+        public int ObjsWritten { get; set; } = 0;
+        public int ParamsWritten { get; set; } = 0;
+
+        public List<Action<int>> ListWriters { get; } = new();
+        public List<Action<int>> ObjWriters { get; } = new();
+        public List<Action<int>> ParamWriters { get; } = new();
+        public List<Action> DataWriters { get; } = new();
+        public List<Action> StringWriters { get; } = new();
+
+        public AampMeta(int ioOffset)
+        {
+            _ioOffset = ioOffset;
+        }
+    }
+}
