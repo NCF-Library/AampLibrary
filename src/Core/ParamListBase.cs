57a45918909bb5b28adcb41540fe653e9c6b1bf2
ArchLeaders <archparliament@gmail.com>
2023-01-07 13:50:18 -0800

    Impl param list and obj base types

---
+++ src/Core/ParamListBase.cs
@@ -0,0 +1,15 @@
+﻿using CeadLibrary.IO;
+
+namespace AampLibrary.Core
+{
+    public abstract class ParamListBase
+    {
+        internal string Name { get; set; } = string.Empty;
+
+        public ParamListBase() { }
+        internal ParamListBase(CeadReader reader) => Read(reader);
+
+        public abstract void Read(CeadReader reader);
+        public abstract void Write(CeadWriter writer, string name, AampMeta meta);
+    }
+}
